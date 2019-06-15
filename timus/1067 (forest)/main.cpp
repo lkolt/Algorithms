c3256097de79e788ef4cb7b03cfaf7517dad707a
lkolt <lkolt2@mail.ru>
2019-06-15 23:49:43 +0300

    solved timus 1067 with forest

---
+++ timus/1067 (forest)/main.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+
+/** Template */
+
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define pb push_back
+#define vi vector <int>
+#define vl vector <ll>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+/** IO Interface */
+
+inline int readChar();
+template <class T = int> inline T readInt();
+template <class T> inline void writeInt( T x, char end = 0 );
+inline void writeChar( int x );
+inline void writeWord( const char *s );
+
+/** Const */
+
+const int maxn = (1.6e5);
+const ll INF = (1e18) + 7;
+const ld EPS = 1e-8;
+
+/** Solution */
+
+struct point {
+    map <string, point*> to;
+    int depth;
+
+    point (int depth) {
+        this->depth = depth;
+    }
+};
+
+vector <string> split (string s) {
+    string word = "";
+    vector <string> res;
+    s += "\\";
+    for (auto ch: s) {
+        if (ch == '\\') {
+            res.push_back(word);
+            word = "";
+        } else {
+            word += ch;
+        }
+    }
+
+    return res;
+}
+
+void add (vector <string> s, int depth, point* root) {
+    if (depth >= s.size()) {
+        return;
+    }
+    string cur = s[depth];
+    point* _to = root->to[cur];
+    if (_to) {
+        add(s, depth + 1, _to);
+    } else {
+        root->to[cur] = new point(depth + 1);
+        add(s, depth + 1, root->to[cur]);
+    }
+}
+
+void print_label_with_spaces (string s, int depth) {
+    for (int i = 0; i < depth; i++) {
+        cout << " ";
+    }
+    cout << s << "\n";
+}
+
+void print_tree (point *root) {
+    for (auto pt: root->to) {
+        print_label_with_spaces(pt.first, root->depth);
+        print_tree(pt.second);
+    }
+}
+
+int main()
+{
+  //  freopen("", "r", stdin);
+  //  freopen("", "w", stdout);
+
+  //  cout << fixed << setprecision(5);
+
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    point* root = new point(0);
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        vector <string> words = split(s);
+        add(words, 0, root);
+    }
+
+    print_tree(root);
+
+	return 0;
+}
+
+/** Helpful funcs*/
+/** Read */
+
+static const int buf_size = 4096;
+
+inline int getChar() {
+	static char buf[buf_size];
+	static int len = 0, pos = 0;
+	if (pos == len)
+		pos = 0, len = fread(buf, 1, buf_size, stdin);
+	if (pos == len)
+		return -1;
+	return buf[pos++];
+}
+
+inline int readChar() {
+	int c = getChar();
+	while (c <= 32)
+		c = getChar();
+	return c;
+}
+
+template <class T>
+inline T readInt() {
+	int s = 1, c = readChar();
+	T x = 0;
+	if (c == '-')
+		s = -1, c = getChar();
+	while ('0' <= c && c <= '9')
+		x = x * 10 + c - '0', c = getChar();
+	return s == 1 ? x : -x;
+}
+
+/** Write */
+
+static int write_pos = 0;
+static char write_buf[buf_size];
+
+inline void writeChar( int x ) {
+	if (write_pos == buf_size)
+		fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+	write_buf[write_pos++] = x;
+}
+
+template <class T>
+inline void writeInt( T x, char end ) {
+	if (x < 0)
+		writeChar('-'), x = -x;
+
+	char s[24];
+	int n = 0;
+	while (x || !n)
+		s[n++] = '0' + x % 10, x /= 10;
+	while (n--)
+		writeChar(s[n]);
+	if (end)
+		writeChar(end);
+}
+
+inline void writeWord( const char *s ) {
+	while (*s)
+		writeChar(*s++);
+}
+
+struct Flusher {
+	~Flusher() {
+		if (write_pos)
+			fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+	}
+} flusher;
